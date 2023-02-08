8ee6c9ccbd5502e8dcd3b15d4a6d20706737d5a7
NopXx <97354576+NopXx@users.noreply.github.com>
2023-02-09 01:45:53 +0700

    update ♻️✅: add page order

---
+++ pages/profile/order/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>order</div>
+</template>
+
+<script>
+export default {
+  name: 'OrderIndex',
+  layout: 'ProfileLayout',
+  middleware: 'auth',
+  data() {
+    return {
+      userfollow: [],
+    }
+  },
+
+  methods: {
+    async getFollow() {
+      try {
+        const respo = await this.$axios.get(`/follow/product`)
+        this.userfollow = respo.data
+      } catch (e) {
+        // eslint-disable-next-line no-console
+        console.log(e)
+      }
+    },
+  },
+}
+</script>
