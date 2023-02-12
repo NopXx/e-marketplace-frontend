68cc34d0b24c330b38c69c0fbc3caba2ef5aa989
NopXx <97354576+NopXx@users.noreply.github.com>
2023-02-12 16:23:16 +0700

    update♻️✅: add page like

---
+++ components/QuickCart.vue
@@ -0,0 +1,318 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div>
+    <v-tooltip bottom>
+      <template #activator="{ on, attrs }">
+        <v-btn
+          text
+          elevation="2"
+          v-bind="attrs"
+          :loading="loading2"
+          v-on="on"
+          @click="
+            checkFollow()
+            checkCart()
+            dialog = true
+          "
+        >
+          <v-icon class="m-2" color="green">mdi-cart-variant</v-icon>
+        </v-btn>
+      </template>
+      <span>เพิ่มเข้าตะกร้า</span>
+    </v-tooltip>
+    <v-dialog
+      v-model="dialog"
+      transition="dialog-bottom-transition"
+      max-width="600"
+    >
+      <v-card>
+        <div v-if="$auth.loggedIn">
+          <v-toolbar color="primary" dark
+            >เพิ่มเข้าตะกร้า
+            <v-spacer></v-spacer>
+            <v-btn icon dark @click="dialog = false">
+              <v-icon>mdi-close</v-icon>
+            </v-btn></v-toolbar
+          >
+
+          <v-card-text>
+            <v-list-item>
+              <v-list-item-content>
+                <v-img v-if="!!img" height="250" contain :src="img"></v-img>
+                <v-list-item-title class="text-h5">{{
+                  title
+                }}</v-list-item-title>
+                <v-list-item-subtitle class="text-body-1 my-3">
+                  ราคา
+                  {{ Number(price.toFixed(1)).toLocaleString() }}
+                </v-list-item-subtitle>
+                <v-list-item-subtitle class="text-body-1 my-3">
+                  จำนวนสินค้าที่มี
+                  {{ Number(number.toFixed(1)).toLocaleString() }}
+                  ชิ้น
+                </v-list-item-subtitle>
+                <!-- <v-list-item-subtitle>
+                  <v-btn
+                    v-if="followProduct.length === 0"
+                    class="mt-2"
+                    dark
+                    color="pink"
+                    @click="followP"
+                  >
+                    ติดตามสินค้า
+                    <v-icon dark right> mdi-cards-heart-outline </v-icon>
+                  </v-btn>
+                  <v-btn
+                    v-else
+                    class="mt-2"
+                    dark
+                    color="pink"
+                    @click="unfollowP"
+                  >
+                    ติดตามสินค้าแล้ว
+                    <v-icon dark right> mdi-cards-heart </v-icon>
+                  </v-btn>
+                </v-list-item-subtitle> -->
+                <v-list-item-subtitle class="text-h6 mt-4">
+                  <v-icon color="green" @click="decrement"> mdi-minus </v-icon>
+
+                  {{ bpm }}
+
+                  <v-icon color="green" @click="increment"> mdi-plus </v-icon>
+                </v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+
+            <v-card-actions>
+              <div v-if="CartProduct.length > 0 && bpm === 0">
+                <v-btn
+                  class="mx-2 mt-n3"
+                  dark
+                  color="error"
+                  @click="deleteCart"
+                >
+                  ลบออกจากตะกร้า
+                  <v-icon dark right> mdi-cart-remove </v-icon>
+                </v-btn>
+              </div>
+              <div v-else-if="CartProduct.length > 0 && bpm != 0">
+                <v-btn
+                  class="mx-2 mt-n3"
+                  dark
+                  color="warning"
+                  @click="updateCart"
+                >
+                  แก้ไขตะกร้า
+                  <v-icon dark right> mdi-cart-variant </v-icon>
+                </v-btn>
+                <v-btn
+                  class="mx-2 mt-n3"
+                  dark
+                  color="error"
+                  @click="deleteCart"
+                >
+                  ลบออกจากตะกร้า
+                  <v-icon dark right> mdi-cart-remove </v-icon>
+                </v-btn>
+              </div>
+              <div v-else>
+                <v-btn class="mx-2 mt-n3" dark color="green" @click="addCart">
+                  เพิ่มเข้าตะกร้า
+                  <v-icon dark right> mdi-cart-variant </v-icon>
+                </v-btn>
+              </div>
+            </v-card-actions>
+          </v-card-text>
+        </div>
+        <div v-else>
+          <v-toolbar color="primary" dark>คำเตือน</v-toolbar>
+          <v-card-text>
+            <div justify="center">
+              <router-link to="/auth" class="text-decoration-none">
+                <v-btn>เข้าสู่ระบบ</v-btn>
+              </router-link>
+            </div>
+          </v-card-text>
+          <v-card-actions class="justify-end">
+            <v-btn text @click="dialog.value = false">Close</v-btn>
+          </v-card-actions>
+        </div>
+      </v-card>
+    </v-dialog>
+    <v-snackbar v-model="snackbar" :timeout="timeout">
+      {{ text }}
+
+      <template #action="{ attrs }">
+        <v-btn color="blue" text v-bind="attrs" @click="snackbar = false">
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
+  </div>
+</template>
+  
+  <script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: '',
+    },
+    price: {
+      type: Number,
+      default: 0,
+    },
+    number: {
+      type: Number,
+      default: 0,
+    },
+    date: {
+      type: String,
+      default: '',
+    },
+    id: {
+      type: Number,
+      default: 0,
+    },
+    img: {
+      type: String,
+      default: '',
+    },
+  },
+  data: () => ({
+    bpm: 1,
+    rating: 4,
+    followProduct: [],
+    CartProduct: [],
+    dialog: false,
+    cart: {
+      product_id: '',
+      item_number: '',
+    },
+    snackbar: false,
+    text: '',
+    timeout: 2000,
+  }),
+  methods: {
+    decrement() {
+      if (this.bpm > 1 && this.CartProduct.length === 0) {
+        this.bpm--
+      }
+      if (this.CartProduct.length > 0) {
+        this.bpm--
+      }
+    },
+    increment() {
+      if (this.bpm < this.number) {
+        this.bpm++
+      }
+    },
+    async followP() {
+      try {
+        const response = await this.$axios.post(`/follow/product/${this.id}`)
+        // eslint-disable-next-line no-console
+        setTimeout(() => {
+          this.checkFollow()
+          this.snackbar = true
+          this.text = 'ติดตามสินค้าแล้ว'
+        }, response)
+      } catch (e) {
+        // eslint-disable-next-line no-console
+        console.log(e)
+      }
+    },
+    async unfollowP() {
+      try {
+        const response = await this.$axios.delete(`/follow/product/${this.id}`)
+        // eslint-disable-next-line no-console
+        setTimeout(() => {
+          this.checkFollow()
+          this.snackbar = true
+          this.text = 'ยกเลิกติดตามสินค้าแล้ว'
+        }, response)
+      } catch (e) {
+        // eslint-disable-next-line no-console
+        console.log(e)
+      }
+    },
+    async checkFollow() {
+      try {
+        const respo = await this.$axios.get(`/follow/product/${this.id}`)
+        // eslint-disable-next-line no-console
+        this.followProduct = respo.data
+      } catch (e) {
+        // eslint-disable-next-line no-console
+        console.log(e)
+      }
+    },
+    async checkCart() {
+      try {
+        const respo = await this.$axios.get(`cart/${this.id}`)
+        // eslint-disable-next-line no-console
+        console.log(respo.data)
+        this.CartProduct = respo.data
+        if (respo.data.length > 0) {
+          respo.data.forEach((val) => {
+            this.bpm = val.item_number
+          })
+        }
+      } catch (e) {
+        // eslint-disable-next-line no-console
+        console.log(e)
+      }
+    },
+    async addCart() {
+      try {
+        this.cart.product_id = this.id
+        this.cart.item_number = this.bpm
+        const respo = await this.$axios.post(`/cart`, this.cart)
+        setTimeout(() => {
+          this.checkCart()
+          this.snackbar = true
+          this.text = 'เพิ่มเข้าตะกร้าแล้ว'
+        }, respo)
+      } catch (e) {
+        // eslint-disable-next-line no-console
+        console.log(e)
+      }
+    },
+    async updateCart() {
+      try {
+        const respo = await this.$axios.patch(
+          `/cart/${this.CartProduct[0].cart_id}`,
+          {
+            item_number: this.bpm,
+          }
+        )
+        setTimeout(() => {
+          this.checkCart()
+          this.snackbar = true
+          this.text = 'แก้ไขข้อมูลแล้ว'
+        }, respo)
+      } catch (e) {
+        // eslint-disable-next-line no-console
+        console.log(e)
+      }
+    },
+    async deleteCart() {
+      try {
+        const respo = await this.$axios.delete(
+          `/cart/${this.CartProduct[0].cart_id}`
+        )
+        setTimeout(() => {
+          this.checkCart()
+          this.snackbar = true
+          this.text = 'ลบข้อมูลแล้ว'
+          this.bpm = 1
+        }, respo)
+      } catch (e) {
+        // eslint-disable-next-line no-console
+        console.log(e)
+      }
+    },
+  },
+}
+</script>
+  
+  <style>
+</style>
--- pages/profile/like/index.vue
+++ pages/profile/like/index.vue
@@ -1,22 +1,163 @@
 <template>
-    <div>
-      {{ userfollow }}
-    </div>
+  <div>
+    <v-card-title>
+      <v-text-field
+        v-model="search"
+        append-icon="mdi-magnify"
+        label="ค้นหาข้อมูล"
+        single-line
+        hide-details
+      ></v-text-field>
+    </v-card-title>
+    <v-data-table
+      :headers="headers"
+      :items="userfollow"
+      :search="search"
+      no-data-text="ไม่มีข้อมูล"
+      class="elevation-1"
+      :loading="loadingdata"
+    >
+      <template #[`item.product_price`]="{ item }">
+        <v-row>
+          <v-col>{{
+            Number(item.product_price.toFixed(1)).toLocaleString()
+          }}</v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <p class="warning--text">
+              เหลือสินค้าอยู่ {{ item.product_number }} ชิ้น
+            </p>
+          </v-col>
+        </v-row>
+      </template>
+      <!-- image -->
+      <template #[`item.image`]="{ item }">
+        <div v-if="!!item['image']">
+          <v-img
+            :src="item['image']"
+            aspect-ratio="1"
+            max-height="125"
+            contain
+          ></v-img>
+        </div>
+      </template>
+      <!-- show default image -->
+      <template #[`item.default_image`]="{ item }">
+        <v-btn
+          v-if="item.default_image == 1"
+          class="mx-2"
+          fab
+          dark
+          small
+          color="green"
+        >
+          <v-icon dark> mdi-heart </v-icon>
+        </v-btn>
+        <v-btn v-else class="mx-2" fab dark small color="primary">
+          <v-icon dark> mdi-minus </v-icon>
+        </v-btn>
+      </template>
+      <!-- show action -->
+      <!-- ----------------------- -->
+      <template #[`item.actions`]="{ item }">
+        <v-row>
+          <v-col cols="12" md="2">
+            <quick-cart
+              :id="item.product_id"
+              :img="item.image"
+              :price="item.product_price"
+              :title="item.product_name"
+              :number="item.product_number"
+          /></v-col>
+          <v-col cols="12" md="2"
+            ><v-tooltip bottom>
+              <template #activator="{ on, attrs }">
+                <v-btn
+                  v-bind="attrs"
+                  text
+                  elevation="2"
+                  v-on="on"
+                  @click="unfollowCilck(item.product_id)"
+                >
+                  <v-icon color="error">mdi-heart-broken-outline</v-icon>
+                </v-btn>
+              </template>
+              <span>เลิกติมตาม</span>
+            </v-tooltip></v-col
+          >
+        </v-row>
+
+        <!-- dialog -->
+        <v-dialog v-model="dialog" max-width="300">
+          <v-card>
+            <v-card-title class="text-h5"> คำเตือน </v-card-title>
+            <v-card-text class="text-body-1">
+              คุณต้องการเลิกติดตาม
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="green darken-1" text @click="dialog = false">
+                ยกเลิก
+              </v-btn>
+              <v-btn color="red darken-1" text @click="unfollowP"> ตกลง </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+        <!--  -->
+      </template>
+    </v-data-table>
+    <v-snackbar v-model="snackbar" :timeout="timeout">
+      {{ text }}
+
+      <template #action="{ attrs }">
+        <v-btn color="blue" text v-bind="attrs" @click="snackbar = false">
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
+  </div>
 </template>
 
 <script>
+import QuickCart from '~/components/QuickCart.vue'
 export default {
   name: 'ProFile',
+  components: { QuickCart },
   layout: 'ProfileLayout',
   middleware: 'auth',
   data() {
     return {
-      userfollow: []
+      userfollow: [],
+      dialog: false,
+      search: '',
+      page: 1,
+      pageCount: 0,
+      itemsPerPage: 1,
+      headers: [
+        {
+          text: 'รูปสินค้า',
+          value: 'image',
+        },
+        {
+          text: 'ชื่อสินค้า',
+          value: 'product_name',
+        },
+        {
+          text: 'ราคา',
+          value: 'product_price',
+        },
+        { text: 'ตัวเลือก', value: 'actions', sortable: false },
+      ],
+      follow_id: '',
+      snackbar: false,
+      text: '',
+      timeout: 2000,
     }
   },
   async created() {
     await this.getFollow()
-  }, 
+  },
   methods: {
     async getFollow() {
       try {
@@ -26,7 +167,28 @@ export default {
         // eslint-disable-next-line no-console
         console.log(e)
       }
-    }
-  }
+    },
+    unfollowCilck(id) {
+      this.dialog = true
+      this.follow_id = id
+    },
+    async unfollowP() {
+      try {
+        const response = await this.$axios.delete(
+          `/follow/product/${this.follow_id}`
+        )
+        // eslint-disable-next-line no-console
+        setTimeout(() => {
+          this.snackbar = true
+          this.text = 'ยกเลิกติดตามสินค้าแล้ว'
+          this.dialog = false
+          this.getFollow()
+        }, response)
+      } catch (e) {
+        // eslint-disable-next-line no-console
+        console.log(e)
+      }
+    },
+  },
 }
 </script>
