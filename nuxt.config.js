493f62892e73db4fc9ca89c8384a05f99c04a656
NopXx <97354576+NopXx@users.noreply.github.com>
2023-08-10 18:35:53 +0700

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -57,7 +57,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'https://e-marketplace-backend-production.up.railway.app/api',
+    baseURL: 'https://cooperative-boa-coat.cyclic.app/api',
     credentials: false,
     headers: {
       Accept: 'application/json'
